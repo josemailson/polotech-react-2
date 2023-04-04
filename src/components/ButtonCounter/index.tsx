546117d508f5a9fa84f544d1d192117437ff9b04
josemailson <jmailsons@gmail.com>
2023-04-03 21:55:39 -0300

    add: ButtonCounter

---
+++ src/components/ButtonCounter/index.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export interface IButtonCounterProps {
+    title: string;
+    active?: boolean;
+
+}
+
+const ButtonCounter = ({ title, active = false }: IButtonCounterProps) => {
+
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+
+    return <button type="button" onClick={handleClick} disabled={!active}>{`${title}: ${count}`}</button>
+};
+
+export default ButtonCounter;
