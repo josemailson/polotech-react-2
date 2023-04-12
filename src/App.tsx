0185b05cb6ca2e3e47c8abaef811ec8bf3637e05
josemailson <jmailsons@gmail.com>
2023-04-11 23:29:54 -0300

    add: GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ButtonCounter from './components/ButtonCounter';
+import { GlobalStyle } from './styles';
 
 function App() {
 
   return (
     <div>
+      <GlobalStyle/>
       <ButtonCounter title="Clique para adicionar elementos a uma lista" />
     </div>
   );
---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        box-sizing: content-box;
+    }
+
+    body{
+        background: #f4fad2;
+        color: #172c3c;
+    }
+`
