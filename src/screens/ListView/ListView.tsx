95aaecf9e426bb7655f6e16254b37084d2ac96c0
josemailson <jmailsons@gmail.com>
2023-04-15 16:44:34 -0300

    add: show label when there are no tasks

--- src/screens/ListView/ListView.tsx
+++ src/screens/ListView/ListView.tsx
@@ -9,71 +9,75 @@ import Header from "../../components/Header/Header";
 import ButtonListView from "../../components/ButtonListView/ButtonListView";
 
 const ListView = () => {
-    const [tasks, setTasks] = useState<ITaskState[]>([]);
-    const [newTaskLabel, setNewTaskLabel] = useState("");
+  const [tasks, setTasks] = useState<ITaskState[]>([]);
+  const [newTaskLabel, setNewTaskLabel] = useState("");
 
-    const handleNewTaskLabelChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setNewTaskLabel(event.target.value);
-      };
-    
-      const addTask = (label: string) => {
-        const id = nanoid();
-        setTasks((tasks) => [...tasks, { id, label: label, isComplete: false }]);
-      };
-    
-      const handleNewTaskKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === "Enter" && newTaskLabel !== "") {
-          addTask(newTaskLabel);
-          setNewTaskLabel("");
-        }
-      };      
-    
-      const updateTaskCompletion = (taskId: string, isComplete: boolean) => {
-        setTasks((tasks) => tasks.map((task) => {
-          if (task.id === taskId) return { ...task, isComplete }
-          return task;
-        })
-        )
-      }
-    
-      const handleTaskCompleteChange = (event: ChangeEvent<HTMLInputElement>, eachTask: ITaskState) => {
-        updateTaskCompletion(eachTask.id, event.target.checked)
-      }
+  const handleNewTaskLabelChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewTaskLabel(event.target.value);
+  };
 
-      const handleClickRemove = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, taskId: string) => {
-        const newTasks = tasks.filter((item) => item.id !== taskId);
-        setTasks(newTasks);
-      };
+  const addTask = (label: string) => {
+    const id = nanoid();
+    setTasks((tasks) => [...tasks, { id, label: label, isComplete: false }]);
+  };
 
-    return (
-        <ListContainer>
-            <Header title={"To Do App"} color={"#ffffff"} />
-            <h2>Total de tarefas: {tasks.length}</h2>
-            <Spacer height="3rem" />
-            <TodoListContainer>
-                {tasks.map((task) => (
-                    <TodoListItem key={task.id}>
-                        <Checkbox
-                            checked={task.isComplete}
-                            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                                handleTaskCompleteChange(event, task)
-                              }/>
-                        <Spacer width={"2rem"} />
-                        {task.isComplete ? (
-                            <StrikethroughLabel>{task.label}</StrikethroughLabel>
-                        ) : (
-                            task.label
-                        )}
-                        <Spacer width={"2rem"} />
-                        <ButtonListView title={"x"} color={"#e63946"} width={"1.8rem"} height={"1.8rem"}disabled={false} onClick={(event) => handleClickRemove(event, task.id)}/>
-                    </TodoListItem>
-                ))}
-            </TodoListContainer>
-            <Spacer height="3rem" />
-            <InputText placeholder={"Adicione uma nova tarefa"} inputColor={"#ffffff"} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyPress} value={newTaskLabel}/>
-            <Spacer height="3rem" />
-        </ListContainer>
-    );
+  const handleNewTaskKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && newTaskLabel !== "") {
+      addTask(newTaskLabel);
+      setNewTaskLabel("");
+    }
+  };
+
+  const updateTaskCompletion = (taskId: string, isComplete: boolean) => {
+    setTasks((tasks) => tasks.map((task) => {
+      if (task.id === taskId) return { ...task, isComplete }
+      return task;
+    })
+    )
+  }
+
+  const handleTaskCompleteChange = (event: ChangeEvent<HTMLInputElement>, eachTask: ITaskState) => {
+    updateTaskCompletion(eachTask.id, event.target.checked)
+  }
+
+  const handleClickRemove = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, taskId: string) => {
+    const newTasks = tasks.filter((item) => item.id !== taskId);
+    setTasks(newTasks);
+  };
+
+  return (
+    <ListContainer>
+      <Header title={"To Do App"} color={"#ffffff"} as="h1"/>
+      <Header title={`Total de tarefas: ${tasks.length}`} color={"#ffffff"} as="h2"/>
+      <Spacer height="3rem" />
+      <TodoListContainer>
+        {tasks.length === 0 ? (
+          <Header title={"Sem tarefas cadastradas"} color={"#ffffff"} as="h2"/>
+        ) : <>{tasks.map((task) => (
+          <TodoListItem key={task.id}>
+            <Checkbox
+              checked={task.isComplete}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                handleTaskCompleteChange(event, task)
+              } />
+            <Spacer width={"2rem"} />
+            {task.isComplete ? (
+              <StrikethroughLabel>{task.label}</StrikethroughLabel>
+            ) : (
+              task.label
+            )}
+            <Spacer width={"2rem"} />
+            <ButtonListView title={"x"} color={"#e63946"} width={"1.8rem"} height={"1.8rem"} disabled={false} onClick={(event) => handleClickRemove(event, task.id)} />
+          </TodoListItem>
+
+        ))}</>}
+
+      </TodoListContainer>
+      <Spacer height="3rem" />
+      <InputText placeholder={"Adicione uma nova tarefa"} inputColor={"#ffffff"} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyPress} value={newTaskLabel} />
+      <Spacer height="3rem" />
+    </ListContainer>
+  );
 };
 
 export default ListView;
