ef64e1a3bb1b723196d5b87cc15f6fbfb28483d5
josemailson <jmailsons@gmail.com>
2023-04-29 20:59:07 -0300

    secret test

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY_SECRET,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
