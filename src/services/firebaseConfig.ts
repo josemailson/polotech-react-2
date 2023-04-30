7c583bdf4378cc77e9a947d66f9075d310a11ff6
josemailson <jmailsons@gmail.com>
2023-04-29 21:17:56 -0300

    using vercel secret for keys

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY_SECRET,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
